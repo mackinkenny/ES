e877ff32f44f136cbc623bf0da6b941772fa8c85
mackinkenny <Mackinkenny@gmail.com>
2018-05-30 16:35:10 +0600

    1.1

--- app/Http/Controllers/OthertypeController.php
+++ app/Http/Controllers/OthertypeController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Other;
+use App\Othertype;
 use Illuminate\Http\Request;
 
 class OthertypeController extends Controller
 {
     //
+    public function createothertype()
+    {
+        $categories = Category::all();
+        return view('create.createothertype',['categories' => $categories]);
+    }
+    public function storeothertype(Request $request)
+    {
+        $other = new Othertype();
+        $other->name = $request->name;
+        $other->category_id = $request->category_id;
+        $other->save();
+
+        return back();
+    }
+    public function othertypesort($id)
+    {
+        $others = Other::where('type_id', '=', $id)->get();
+
+        return response()->json(['others' => $others]);
+    }
+
+    public function othertypebacksort()
+    {
+        $others = Other::all();
+
+        return response()->json(['others' => $others]);
+    }
 }
