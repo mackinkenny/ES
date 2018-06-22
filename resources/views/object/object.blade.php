822161015d15dcdc8cdd839d2b9ff6a41826ff95
mackinkenny <Mackinkenny@gmail.com>
2018-06-22 15:08:54 +0600

    contact3

--- resources/views/object/object.blade.php
+++ resources/views/object/object.blade.php
@@ -36,14 +36,15 @@
                 <div class="col-3 object-par">Цена:</div>
                 <div class="col-9 object-par">{{$object->price}}</div>
                 @endif
-                @if($object->contact)
-                <div class="col-3 object-par">Контакты: </div>
-                <div class="col-9 object-par">{{$object->contact}}</div>
-                    @endif
+
                     @if($object->address)
                 <div class="col-3 object-par">Адресс:</div>
                 <div class="col-9 object-par">{{$object->address}}</div>
                         @endif
+                    @if($object->contact)
+                        <div class="col-3 object-par">Контакты: </div>
+                        <div class="col-9 object-par">{{$object->contact}}</div>
+                    @endif
 
             </div><hr>
                 <div class="data p-2">
