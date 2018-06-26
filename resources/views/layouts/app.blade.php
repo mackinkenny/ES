03517b479b7b09f06404561de5af12ee8d6c61be
mackinkenny <Mackinkenny@gmail.com>
2018-06-26 17:39:42 +0600

    mobile 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
 
     <meta charset="UTF-8">
     <title>Almash</title>
--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -5,12 +5,12 @@
 
     {{--</div>--}}
 <div class="container">
-    <p class="text-center mt-3" style="font-size:35px; font-weight: bold; font-family: 'Times New Roman'; letter-spacing: 2px;">Категории</p><hr>
-        <div class="row text-center justify-content-center p-5 w-100" style="background:white;">
+    <p class="text-center mt-3 title-text" style="font-size:35px; font-weight: bold; font-family: 'Times New Roman'; letter-spacing: 2px;">Категории</p><hr>
+        <div class="row text-center justify-content-center p-lg-5 w-100" style="background:white;">
             @foreach($categories as $category)
         <div class="col-4 cat">
                     <a href="/show/{{$category->id}}">
-                    <img class="pt-2" src="/image/category/{{$category->img_path}}" alt="">
+                    <img class="pt-2 cat-img" src="/image/category/{{$category->img_path}}" alt="">
                     <br>
                     <br>
                     <p class="cat-text">{{$category->text}}</p>
@@ -21,13 +21,13 @@
 </div>
     <div class="container">
         <hr>
-        <div style="font-size:35px;text-align:center; font-weight: bolder; font-family: 'Times New Roman'; letter-spacing: 2px;">Цель нашего сайта</div><hr>
-        <div class="col-12 row mt-5">
-            <div class="col-6" style="margin-top: 100px;">
-        <iframe width="510px" height="270px" src="https://www.youtube.com/embed/hgTKRmtkbBM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <div class="title-text" style="font-size:35px;text-align:center; font-weight: bolder; font-family: 'Times New Roman'; letter-spacing: 2px;">Цель нашего сайта</div><hr>
+        <div class="row mt-lg-5 p-2">
+            <div class="col-md-6 ml-lg-5" >
+        <iframe class="main-video" src="https://www.youtube.com/embed/hgTKRmtkbBM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
-            <div class="col-5 ml-5">
-                <div class="text-center">
+            <div class="col-md-5">
+                <div class="text-center mt-4 mt-md-0">
                     <a href="http://diamondresort.kg/"><img src="/image/logo_test.png" class="w-50" style="margin-bottom: 20px;" alt=""></a>
                 </div>
                 <span style="font-size:17px; font-family: Forum;">
@@ -39,7 +39,7 @@
 
                 </span>
             </div>
-            <div class="col-12 p-5">
+            <div class="col-12 mt-5">
                 <span style="font-size:17px; font-family: Forum;">
                     Цель нашего сайта - это поддержание нашего объекта <strong>"Diamond Resort"</strong>. Все наши товары выставленные на портале используются для
                     обмена на нужные нам материалы и товары для строительства нашего городка. Если вы заинтересованы в наших товарах, вы можете оставить нам свое предложение во вкладке <strong>"Оставить предложение"</strong> и мы с вами свяжемся.
@@ -53,45 +53,44 @@
     <hr>
     <div class="row col-12 justify-content-center mt-5">
             @foreach($products as $product)
-            <div class="row col-5 p-4 mb-2 mr-5" style="background:#f9f9f9;">
+            <div class="row col-md-5 p-md-4 p-2 mb-2 mr-md-5 ml-3" style="background:#f9f9f9;">
                 @if($product->img_path != null)
-                <div class="col-4">
-                    <a href="/showproduct/{{$product->id}}"><div class="img-prod" style="background-image: url('/../image/photo/{{$product->img_path}}'); height:150px!important;">
+                <div class="col-md-4 col-3 text-script">
+                    <a href="/showproduct/{{$product->id}}"><div class="img-prod" style="background-image: url('/../image/photo/{{$product->img_path}}');">
                             @if($product->our == 1)
-                                <img src="/image/Almash.png" style="position: absolute;top:-35px; left:-15%; width:70px; height:70px; transform: rotate(-20deg)" alt="">
+                                <img class="stamp" src="/image/main.png" style="position: absolute;top:-35px; left:-15%; width:70px; height:70px; transform: rotate(-20deg)" alt="">
                             @endif
                         </div></a>
 
                 </div>
-
-                <div class="col-8">
-                    <div class="col-12">
-                        <a href="/showproduct/{{$product->id}}" class="main-text ml-3"><?= str_limit($product->name, 30) ?></a><hr>
-                    </div>
+                <div class="col-8 text-script">
+                    <div class="col-12 text-script">
+                        <a href="/showproduct/{{$product->id}}" class="main-text ml-md-3"><?= str_limit($product->name, 30) ?></a>
+                    </div><br>
                     <div class="col-12 text-script">
 
-                        <span class="ml-3 mr-5">{{$product->parent_name}}</span>
+                        <span class="ml-md-3 mr-md-5 mr-4">{{$product->parent_name}}</span>
 
-                        <span class="mr-5">{{$product->created_at}}</span><hr>
-                    </div>
-                    <div class="col-12 ml-3 mt-2 text-script">
+                        <span>{{$product->created_at}}</span>
+                    </div><br>
+                    <div class="col-12 ml-md-3 text-script">
                         <span><?= str_limit($product->info, 80) ?></span>
                     </div>
                 </div>
                     @else
                         <div class="col-12">
                             @if($product->our == 1)
-                                <img src="/image/Almash.png" style="position: absolute;top:-35px; left:-5%; width:70px; height:70px; transform: rotate(-20deg)" alt="">
+                                <img class="stamp" src="/image/main.png" style="position: absolute;top:-35px; left:-5%; width:70px; height:70px; transform: rotate(-20deg)" alt="">
                             @endif
-                            <a href="/showproduct/{{$product->id}}" class="main-text ml-3"><?= str_limit($product->name, 30) ?></a><hr>
-                        </div>
+                            <a href="/showproduct/{{$product->id}}" class="main-text ml-md-3"><?= str_limit($product->name, 30) ?></a>
+                        </div><br>
                         <div class="col-12 text-script">
 
-                            <span class="ml-3 mr-5">{{$product->parent_name}}</span>
+                            <span class="ml-md-3 mr-5">{{$product->parent_name}}</span>
 
-                            <span class="mr-5">{{$product->created_at}}</span><hr>
-                        </div>
-                        <div class="col-12 ml-3 mt-2 text-script">
+                            <span class="mr-5">{{$product->created_at}}</span>
+                        </div><br>
+                        <div class="col-12 ml-md-3 mt-2 text-script">
                             <span><?= str_limit($product->info, 80) ?></span>
                         </div>
                 @endif
