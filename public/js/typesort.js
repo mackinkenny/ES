aea462123e89bdd6adfed8cd0e308633e821d67e
mackinkenny <Mackinkenny@gmail.com>
2018-06-26 12:16:08 +0600

    js end i hope

--- public/js/typesort.js
+++ public/js/typesort.js
@@ -36,29 +36,73 @@ $(document).ready(function() {
                             product.name = product.name.slice(0, 45) + "...";
                         }
                         if(product.img_path != null) {
-                            $('#ajax-types').append(
-                                "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
-                                "<div class='col-4'>" +
-                                "<a href='/showproduct/" + product.id + "'><div class='img-prod' style='background-image: url(/../image/photo/" + product.img_path + ");'></div></a>" +
-                                "</div>" +
-                                "<div class='col-8'>" +
-                                "<div class='col-12'>" +
-                                "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
-                                "</div>" +
-                                "<div class='col-12 text-script'>" +
-                                "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
-                                "<span class='mr-5'>" + product.created_at + "</span>" +
-                                "</div>" +
-                                "<div class='col-12 ml-3 mt-2 text-script'>" +
-                                "<span>" + product.info + "</span>" +
-                                "</div>" +
-                                "</div>" +
-                                "</div>"
-                            );
+                            if(product.our == 1)
+                            {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-4'>" +
+                                    "<a href='/showproduct/" + product.id + "'><div class='img-prod' style='background-image: url(/../image/photo/" + product.img_path + ");'></div></a>" +
+                                    "<img src='/image/Almash.png' style='position: absolute;top:-35px; left:-15%; width:70px; height:70px; transform: rotate(-20deg)' alt=''>" +
+                                    "</div>" +
+                                    "<div class='col-8'>" +
+                                    "<div class='col-12'>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
+                            else {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-4'>" +
+                                    "<a href='/showproduct/" + product.id + "'><div class='img-prod' style='background-image: url(/../image/photo/" + product.img_path + ");'></div></a>" +
+                                    "</div>" +
+                                    "<div class='col-8'>" +
+                                    "<div class='col-12'>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
                         }
                         else
                         {
-                            $('#ajax-types').append(
+                            if(product.our == 1) {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-12'>" +
+                                    "<img src='/image/Almash.png' style='position: absolute;top:-35px; left:-5%; width:70px; height:70px; transform: rotate(-20deg)' alt=''>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
+                            else
+                            {
+                                $('#ajax-types').append(
                                 "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
                                 "<div class='col-12'>" +
                                 "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
@@ -71,7 +115,8 @@ $(document).ready(function() {
                                 "<span>" + product.info + "</span>" +
                                 "</div>" +
                                 "</div>"
-                            );
+                                );
+                            }
                         }
 
                         $('#ajax-types').show(100);
@@ -115,43 +160,89 @@ $(document).ready(function() {
                             product.name = product.name.slice(0, 45) + "...";
                         }
                         if(product.img_path != null) {
-                            $('#ajax-types').append(
-                                "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
-                                "<div class='col-4'>" +
-                                "<a href='/showproduct/" + product.id + "'><div class='img-prod' style='background-image: url(/../image/photo/" + product.img_path + ");'></div></a>" +
-                                "</div>" +
-                                "<div class='col-8'>" +
-                                "<div class='col-12'>" +
-                                "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
-                                "</div>" +
-                                "<div class='col-12 text-script'>" +
-                                "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
-                                "<span class='mr-5'>" + product.created_at + "</span>" +
-                                "</div>" +
-                                "<div class='col-12 ml-3 mt-2 text-script'>" +
-                                "<span>" + product.info + "</span>" +
-                                "</div>" +
-                                "</div>" +
-                                "</div>"
-                            );
+                            if(product.our == 1)
+                            {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-4'>" +
+                                    "<a href='/showproduct/" + product.id + "'><div class='img-prod' style='background-image: url(/../image/photo/" + product.img_path + ");'></div></a>" +
+                                    "<img src='/image/Almash.png' style='position: absolute;top:-35px; left:-15%; width:70px; height:70px; transform: rotate(-20deg)' alt=''>" +
+                                    "</div>" +
+                                    "<div class='col-8'>" +
+                                    "<div class='col-12'>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
+                            else {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-4'>" +
+                                    "<a href='/showproduct/" + product.id + "'><div class='img-prod' style='background-image: url(/../image/photo/" + product.img_path + ");'></div></a>" +
+                                    "</div>" +
+                                    "<div class='col-8'>" +
+                                    "<div class='col-12'>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
                         }
                         else
                         {
-                            $('#ajax-types').append(
-                                "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
-                                "<div class='col-12'>" +
-                                "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
-                                "</div>" +
-                                "<div class='col-12 text-script'>" +
-                                "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
-                                "<span class='mr-5'>" + product.created_at + "</span>" +
-                                "</div>" +
-                                "<div class='col-12 ml-3 mt-2 text-script'>" +
-                                "<span>" + product.info + "</span>" +
-                                "</div>" +
-                                "</div>"
-                            );
+                            if(product.our == 1) {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-12'>" +
+                                    "<img src='/image/Almash.png' style='position: absolute;top:-35px; left:-5%; width:70px; height:70px; transform: rotate(-20deg)' alt=''>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
+                            else
+                            {
+                                $('#ajax-types').append(
+                                    "<div class='row col-12 p-4 ml-1 mb-2' style='background:#f9f9f9;'>" +
+                                    "<div class='col-12'>" +
+                                    "<a href='/showproduct/" + product.id + "' class='main-text ml-3'>" + product.name + "</a><hr>" +
+                                    "</div>" +
+                                    "<div class='col-12 text-script'>" +
+                                    "<span class='ml-3 mr-5'>" + product.parent_name + "</span>" +
+                                    "<span class='mr-5'>" + product.created_at + "</span>" +
+                                    "</div>" +
+                                    "<div class='col-12 ml-3 mt-2 text-script'>" +
+                                    "<span>" + product.info + "</span>" +
+                                    "</div>" +
+                                    "</div>"
+                                );
+                            }
                         }
+
                         $('#ajax-types').show(100);
                     }
                 }
